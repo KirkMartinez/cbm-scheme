6733414449a3f3afc72d0131647208ed65801687
Kirk Martinez <kirk.martinez@gmail.com>
2015-11-10 12:49:53 -0800

    add symbols, shorten error messages

--- scheme.c
+++ scheme.c
@@ -8,27 +8,32 @@
 
 /************************ MODEL ************************/
 
-typedef enum {THE_EMPTY_LIST, BOOLEAN, FIXNUM, CHARACTER, STRING, PAIR} object_type;
+typedef enum {THE_EMPTY_LIST, BOOLEAN, SYMBOL, FIXNUM, 
+              CHARACTER, STRING, PAIR} object_type;
 
 typedef struct object {
     object_type type;
+
     union {
-		struct {
-			char value;
-		} boolean;
         struct {
-            int value; // was long, but that crashed VICE
+            char value;
+        } boolean;
+        struct {
+            char *value;
+        } symbol;
+        struct {
+            int value;
         } fixnum;
-		struct {
-			char value;
-		} character;
-		struct {
-			char *value;
-		} string;
-		struct {
-			struct object *car;
-			struct object *cdr;
-		} pair;
+        struct {
+            char value;
+        } character;
+        struct {
+            char *value;
+        } string;
+        struct {
+            struct object *car;
+            struct object *cdr;
+        } pair;
     } data;
 } object;
 
@@ -43,49 +48,92 @@ object *alloc_object(void) {
     return obj;
 }
 
+// Globals
+object *the_empty_list;
+object *false;
+object *true;
+object *symbol_table;
+
+// Forwards
+object *cons(object *car, object *cdr);
+object *car(object *pair);
+object *cdr(object *pair);
+
+void init(void) {
+    getc_conio_init();
+
+    the_empty_list = alloc_object();
+    the_empty_list->type = THE_EMPTY_LIST;
+
+    false = alloc_object();
+    false->type = BOOLEAN;
+    false->data.boolean.value = 0;
+
+    true = alloc_object();
+    true->type = BOOLEAN;
+    true->data.boolean.value = 1;
+
+    symbol_table = the_empty_list;
+}
+
 // EMPTY LIST
 
-object *the_empty_list;
-
 char is_the_empty_list(object *obj) {
-	return obj == the_empty_list;
+    return obj == the_empty_list;
 }
 
 // BOOLEAN
 
-object *false;
-object *true;
-
 char is_boolean(object *obj) {
-	return obj->type == BOOLEAN;
+    return obj->type == BOOLEAN;
 }
 
 char is_false(object *obj) {
-	return obj == false;
+    return obj == false;
 }
 
 char is_true(object *obj) {
-	return !is_false(obj);
+    return !is_false(obj);
 }
 
-void init(void) {
-	getc_conio_init();
+// SYMBOL
 
-	the_empty_list = alloc_object();
-	the_empty_list->type = THE_EMPTY_LIST;
+object *make_symbol(char *value) {
+    object *obj;
+    object *element;
 
-	false = alloc_object();
-	false->type = BOOLEAN;
-	false->data.boolean.value = 0;
-
-	true = alloc_object();
-	true->type = BOOLEAN;
-	true->data.boolean.value = 1;
+    // TODO: re-implement using a hash table...for performance
+    
+    /* search for they symbol in the symbol table */
+    element = symbol_table;
+    while (!is_the_empty_list(element)) {
+        if (strcmp(car(element)->data.symbol.value, value) == 0) {
+            return car(element);
+        }   
+        element = cdr(element);
+    };
+    
+    /* create the symbol and add it to the symbol table */
+    obj = alloc_object();
+    obj->type = SYMBOL;
+    obj->data.symbol.value = malloc(strlen(value) + 1); 
+    if (obj->data.symbol.value == NULL) {
+        fprintf(stderr, "out of memory\n");
+        exit(1);
+    }   
+    strcpy(obj->data.symbol.value, value);
+    symbol_table = cons(obj, symbol_table);
+    return obj;
 }
+
+char is_symbol(object *obj) {
+    return obj->type == SYMBOL;
+}
+
 
 // FIXNUM
 
-object *make_fixnum(int value) { // was long
+object *make_fixnum(int value) { 
     object *obj;
 
     obj = alloc_object();
@@ -95,7 +143,7 @@ object *make_fixnum(int value) { // was long
 }
 
 char is_fixnum(object *obj) {
-	return obj->type == FIXNUM;
+    return obj->type == FIXNUM;
 }
 
 // CHARACTER
@@ -123,7 +171,7 @@ object *make_string(char *value) {
     obj->data.string.value = malloc(strlen(value) + 1); 
     if (obj->data.string.value == NULL) {
         fprintf(stderr, "out of memory\n");
-		exit(1);
+        exit(1);
     }   
     strcpy(obj->data.string.value, value);
     return obj;
@@ -147,23 +195,23 @@ object *cons(object *car, object *cdr) {
 }
 
 char is_pair(object *obj) {
-	return obj->type == PAIR;
+    return obj->type == PAIR;
 }
 
 object *car(object *pair) {
-	return pair->data.pair.car;
+    return pair->data.pair.car;
 }
 
 void set_car(object *obj, object *value) {
-	obj->data.pair.car = value;
+    obj->data.pair.car = value;
 }
 
 object *cdr(object *pair) {
-	return pair->data.pair.cdr;
+    return pair->data.pair.cdr;
 }
 
 void set_cdr(object *obj, object *value) {
-	obj->data.pair.cdr = value;
+    obj->data.pair.cdr = value;
 }
 
 /*********************** READ ***********************/
@@ -176,12 +224,11 @@ char peek(void) {
     return c;
 }
 
-// this assumes we've read the entire program in
-// which will be problematic at some point...
+// consume spaces until a non-space char is seen
 void eat_whitespace(void) {
     char c;
 
-	// For now, stop reading on newline, not EOF
+    // For now, stop reading on newline, not EOF
     while ((c = getc_conio()) != '\n') {
         if (isspace(c)) {
             continue;
@@ -197,8 +244,13 @@ void eat_whitespace(void) {
 
 char is_delimiter(int c) {
     return isspace(c) || c == GETC_CONIO_EOF ||
-		c == '('   || c == ')' ||
-		c == '"'   || c == ';';
+        c == '('   || c == ')' ||
+        c == '"'   || c == ';';
+}
+
+char is_initial(int c) {
+    return isalpha(c) || c == '*' || c == '/' || c == '>' ||
+             c == '<' || c == '=' || c == '?' || c == '!';
 }
 
 void eat_expected_string(char *str) {
@@ -207,8 +259,8 @@ void eat_expected_string(char *str) {
     while (*str != '\0') {
         c = getc_conio();
         if (c != *str) {
-            fprintf(stderr, "unexpected character '%c'\n", c);
-			exit(1);
+            fprintf(stderr, "unexpected '%c'\n", c);
+            exit(1);
         }
         str++;
     }
@@ -216,8 +268,8 @@ void eat_expected_string(char *str) {
 
 void peek_expected_delimiter(void) {
     if (!is_delimiter(peek())) {
-        fprintf(stderr, "character not followed by delimiter\n");
-		exit(1);
+        fprintf(stderr, "no delim after char\n");
+        exit(1);
     }
 }
 
@@ -227,8 +279,8 @@ object *read_character(void) {
     c = getc_conio();
     switch (c) {
         case EOF:
-            fprintf(stderr, "incomplete character literal\n");
-			exit(1);
+            fprintf(stderr, "early EOF\n");
+            exit(1);
         case 's':
             if (peek() == 'p') {
                 eat_expected_string("pace");
@@ -271,14 +323,14 @@ object *read_pair(void) {
     if (c == '.') { /* read improper list */
         c = peek();
         if (!is_delimiter(c)) {
-            fprintf(stderr, "dot not followed by delimiter\n");
+            fprintf(stderr, "no delimn after dot\n");
             exit(1);
         }   
         cdr_obj = read();
         eat_whitespace();
         c = getc_conio();
         if (c != ')') {
-            fprintf(stderr, "missing close paren\n");
+            fprintf(stderr, "missing (\n");
             exit(1);
         }   
 
@@ -291,51 +343,78 @@ object *read_pair(void) {
     }   
 }
 
+// the main read function
 object *read(void) {
-	char c;
-	short sign = 1;
+    char c;
+    short sign = 1;
     short i=0;
     int num = 0;
-#define BUFFER_MAX 100
-	char buffer[BUFFER_MAX];
+#define BUFFER_MAX 10
+    char buffer[BUFFER_MAX];
 
-	eat_whitespace();
+    eat_whitespace();
 
-	c = getc_conio();
+    c = getc_conio();
 
-	if (c == '#') { // BOOLEAN or CHARACTER
-		c = getc_conio();
-		switch (c) {
-			case 't':
-				return true;
-			case 'f':
-				return false;
-			case '%':
-				return read_character();
-			default:
-				fprintf(stderr, "Unknown boolean literal\n");
-				exit(1);
-		}
-	} else if (isdigit(c) || (c == '-' && (isdigit(peek())))) {
-		/* read a fixnum */
-		if (c == '-') {
-			sign = -1;
-			++i;
-		} else {
-			ungetc_conio(c);
-		} 
-		while (isdigit(c=getc_conio())) {
-			num = (num * 10) + (c - '0');
-		}
-		num *= sign;
-		if (is_delimiter(c)) {
-			ungetc_conio(c);
-			return make_fixnum(num);
-		} else {
-			fprintf(stderr, "number not followed by delimiter\n");
-			exit(1);
-		}
-	} else if (c == '"') { // STRING
+    if (c == '#') { // BOOLEAN or CHARACTER
+        c = getc_conio();
+        switch (c) {
+            case 't':
+                return true;
+            case 'f':
+                return false;
+            case '%':
+                return read_character();
+            default:
+                fprintf(stderr, "Unknown literal\n");
+                exit(1);
+        }
+    } else if (isdigit(c) || (c == '-' && (isdigit(peek())))) {
+        // FIXNUM
+        if (c == '-') {
+            sign = -1;
+            ++i;
+        } else {
+            ungetc_conio(c);
+        } 
+        while (isdigit(c=getc_conio())) {
+            num = (num * 10) + (c - '0');
+        }
+        num *= sign;
+        if (is_delimiter(c)) {
+            ungetc_conio(c);
+            return make_fixnum(num);
+        } else {
+            fprintf(stderr, "no delim after number\n");
+            exit(1);
+        }
+    } else if (is_initial(c) ||
+             ((c == '+' || c == '-') &&
+              is_delimiter(peek()))) { // SYMBOL
+        i = 0;
+        while (is_initial(c) || isdigit(c) ||
+               c == '+' || c == '-') {
+            /* subtract 1 to save space for '\0' terminator */
+            if (i < BUFFER_MAX - 1) {
+                buffer[i++] = c;
+            }
+            else {
+                fprintf(stderr, "symbol exceeds %d chars\n", BUFFER_MAX-1);
+                exit(1);
+            }
+            c = getc_conio();
+        }
+        if (is_delimiter(c)) {
+            buffer[i] = '\0';
+            ungetc_conio(c);
+            return make_symbol(buffer);
+        }
+        else {
+            fprintf(stderr, "no delimn after symbol"
+                            "Found '%c'\n", c);
+            exit(1);
+        }
+    } else if (c == '"') { // STRING
         i = 0;
         while ((c = getc_conio()) != '"') {
             if (c == '%') {
@@ -345,35 +424,35 @@ object *read(void) {
                 }
             }
             if (c == 0) { // readline returns zero for EOF
-                fprintf(stderr, "non-terminated string literal\n");
-				exit(1);
+                fprintf(stderr, "non-terminated string\n");
+                exit(1);
             }
             /* subtract 1 to save space for '\0' terminator */
             if (i < BUFFER_MAX - 1) {
                 buffer[i++] = c;
             }
             else {
-                fprintf(stderr, "string too long. Maximum length is %d\n", BUFFER_MAX);
-				exit(1);
+                fprintf(stderr, "string exceeds %d chars\n", BUFFER_MAX);
+                exit(1);
             }
         }
         buffer[i] = '\0';
         return make_string(buffer);
-	} else if (c == '(') { // empty list or pair
-		return read_pair();
-	} else {
-		fprintf(stderr, "bad input. Unexpected '%c'\n", c);
-		exit(1);
-	}
-	fprintf(stderr, "read illegal state\n");
-	exit(1);
+    } else if (c == '(') { // THE_EMPTY_LIST or PAIR
+        return read_pair();
+    } else { 
+        fprintf(stderr, "bad input. Unexpected '%c'\n", c);
+        exit(1);
+    }
+    fprintf(stderr, "input error\n");
+    exit(1);
 }
 
 /************************ EVAL ************************/
 
 /* until we have lists and symbols just echo */
 object *eval(object *exp) {
-	return exp;
+    return exp;
 }
 
 /************************ PRINT ************************/
@@ -402,91 +481,94 @@ void write_pair(object *pair) {
 }
 
 void writeit(object *obj) {
-	char c;
-	char *str;
+    char c;
+    char *str;
 
-	switch (obj->type) {
-		case THE_EMPTY_LIST:
-			printf("()");
-			break;
-		case BOOLEAN:
-			printf("#%c", is_false(obj) ? 'f' : 't');
-			break;
-		case FIXNUM:
-			printf("%d", obj->data.fixnum.value);
-			break;
-		case CHARACTER:
-			c = obj->data.character.value;
-			printf("#'");
-			switch (c) {
-				case '\n':
-					printf("newline");
-					break;
-				case ' ':
-					printf("space");
-					break;
-				default:
-					putchar(c);
-			}
-			break;
-		case STRING:
-			str = obj->data.string.value;
-			putchar('"');
-			while (*str != '\0') {
-				switch (*str) {
-					case '\n':
-						printf("\\n");
-						break;
-					case '\'':
-						printf("''");
-						break;
-					case '"':
-						printf("'");
-						break;
-					default:
-						putchar(*str);
-				}
-				++str;
-			}
-			putchar('"');
-			break;
-		case PAIR:
-			printf("(");
-			write_pair(obj);
-			printf(")");
-			break;
-		default:
-			fprintf(stderr, "cannot write unknown type\n");
-			exit(1);
-	}
+    switch (obj->type) {
+        case THE_EMPTY_LIST:
+            printf("()");
+            break;
+        case BOOLEAN:
+            printf("#%c", is_false(obj) ? 'f' : 't');
+            break;
+        case SYMBOL:
+            printf("%s", obj->data.symbol.value);
+            break;
+        case FIXNUM:
+            printf("%d", obj->data.fixnum.value);
+            break;
+        case CHARACTER:
+            c = obj->data.character.value;
+            printf("#'");
+            switch (c) {
+                case '\n':
+                    printf("newline");
+                    break;
+                case ' ':
+                    printf("space");
+                    break;
+                default:
+                    putchar(c);
+            }
+            break;
+        case STRING:
+            str = obj->data.string.value;
+            putchar('"');
+            while (*str != '\0') {
+                switch (*str) {
+                    case '\n':
+                        printf("\\n");
+                        break;
+                    case '\'':
+                        printf("''");
+                        break;
+                    case '"':
+                        printf("'");
+                        break;
+                    default:
+                        putchar(*str);
+                }
+                ++str;
+            }
+            putchar('"');
+            break;
+        case PAIR:
+            printf("(");
+            write_pair(obj);
+            printf(")");
+            break;
+        default:
+            fprintf(stderr, "cannot write unknown type\n");
+            exit(1);
+    }
 }
 
 /************************ REPL *************************/
 
 void scheme(void) {
-	printf("Welcome to Scheme for Commodore!\n");
+    printf("Welcome to Scheme for Commodore!\n");
 
-	init();
+    init();
 
-	while (1) {
-		printf("> ");
-		writeit(eval(read()));
-		printf("\n");
-	}   
+    while (1) {
+        printf("> ");
+        writeit(eval(read()));
+        printf("\n");
+    }   
 }
 
 int main (void)
 {
-	clrscr (); 
-	textcolor (COLOR_WHITE);
-	bordercolor (COLOR_BLACK);
-	bgcolor (COLOR_BLACK);
+    clrscr (); 
+    textcolor (COLOR_WHITE);
+    bordercolor (COLOR_BLACK);
+    bgcolor (COLOR_BLACK);
 
-	scheme();
+    scheme();
 
-	printf("\nPress a key to exit.\n");
-	cgetc(); 
+    printf("\nPress a key to exit.\n");
+    cgetc(); 
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
